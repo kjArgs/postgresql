803306dd0ee8aff8d5aa98df36b572f6f9d71b6c
Kristan James Aragon <aragonjames111@gmail.com>
2026-06-30 18:08:53 +0800

    fix:  refactor database connection to use Kysely with PostgresDialect

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,16 +1,20 @@
 import pg from "pg";
 import dotenv from "dotenv";
+import { Kysely, PostgresDialect } from "kysely";
+import { Database } from "./types";
 
 dotenv.config();
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+export const db = new Kysely<Database>({
+  dialect: new PostgresDialect({
+    pool: new Pool({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+    }),
+  }),
 });
-
-export default pool;
